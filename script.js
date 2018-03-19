5e853f3c5e4cefc73bcbcd63b95852155a12c362
Allan Liebold <allan.liebold@gmail.com>
2018-03-18 20:16:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ console.log(age);
 
 var name = 'John';
 var age = 26;
+var married = false;
+
+if (married) {
+  console.log(name + ' is married'); 
+} else {
+  console.log(name + ' is not married'); 
+}
