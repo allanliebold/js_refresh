82e192d70f14f81e38db026d768643b048ac6e6d
Allan Liebold <allan.liebold@gmail.com>
2018-03-21 00:20:32 -0700

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -4,5 +4,6 @@ var years = new Array(1990, 1985, 1950);
 console.log(name[0] + ' was born in ' + years[0]);
 
 var types = [true, 10, 'string'];
-types.push('blue');
+types.push('blue'); // adds element at end of array
 console.log(types);
+types.unshift(5); // adds element at beginning of array
