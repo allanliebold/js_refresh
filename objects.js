052fe4aaaa2ce6e8f2426fc74f5c17e88a262fbe
Allan Liebold <allan.liebold@gmail.com>
2018-03-21 00:32:41 -0700

    Update objects.js

--- objects.js
+++ objects.js
@@ -10,11 +10,13 @@ var bob =  {
   married: false
 };
 
+// accessing data in object
 console.log(bob.lastName);
 console.log(bob['lastName']); // bracket notation uses string for key name
 
 var xyz = 'job';
 console.log(bob[xyz]); // returns 'teacher'
 
+// mutating data in object
 bob.name = 'Robert';
 bob[xyz] = 'developer';
