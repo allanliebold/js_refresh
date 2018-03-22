d4feb24a67e0c1b5d6c04e5b95ba510937ebfc45
Allan Liebold <allan.liebold@gmail.com>
2018-03-22 00:10:43 -0700

    Update coding_challenge2.js

--- coding_challenge2.js
+++ coding_challenge2.js
@@ -1,1 +1,18 @@
+var years = [1977, 1980, 1991, 1958];
+var ages = []
 
+function calculateAge(year) {
+  return 2018 - year;  
+}
+
+for (i = 0; i < years.length; i++) {
+  ages.push(calculateAge(years[i]));  
+}
+
+for (i = 0; i < ages.length; i++) {
+  if (ages[i] > 18) {
+    console.log('Person ' + (i+1) + ' is an adult.'); 
+  } else {
+    console.log(' Person ' + (i+1) + ' is a minor.'); 
+  }
+}
