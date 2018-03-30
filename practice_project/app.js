cf223f563d8f77c789e93417e297426ef6cbe534
Allan Liebold <allan.liebold@gmail.com>
2018-03-30 06:06:48 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -5,7 +5,6 @@ roundScore = 0;
 activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
 document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
