e2a3e2e5a28f0b4be040841e860abacbdb368396
Allan Liebold <allan.liebold@gmail.com>
2018-04-01 11:21:04 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -32,6 +32,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   if(scores[activePlayer] >= 100) {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
   } else {
     nextPlayer();
   }
