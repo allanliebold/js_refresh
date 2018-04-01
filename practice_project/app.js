3030cf1cf20a3b8fc53c7bd8649dde28e4e59ae0
Allan Liebold <allan.liebold@gmail.com>
2018-04-01 13:18:47 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -1,11 +1,6 @@
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
 document.querySelector('.dice').style.display = 'none';
-
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
