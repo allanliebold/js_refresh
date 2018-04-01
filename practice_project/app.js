3b2e0cf81357e2df3721a3bd40b312b9b81e3915
Allan Liebold <allan.liebold@gmail.com>
2018-03-31 21:15:16 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -15,7 +15,7 @@ document.querySelector('.btn-roll').addEventListener('click', btn);
 */
 
 document.getElementById('.score-0').textContent = '0';
-document.getElementById('.score-1')
+document.getElementById('.score-1').textContent = '0';
 
 // anonymous function
 
