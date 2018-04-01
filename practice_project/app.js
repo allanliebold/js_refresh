ec2346ea81fcc676cfa0b59af8377cfac3d79a9e
Allan Liebold <allan.liebold@gmail.com>
2018-04-01 11:11:49 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -28,6 +28,7 @@ document.querySelector('.btn-roll').addEvenListener('click', function() {
 document.querySelector('.btn-hold').addEventListener('click', function() {
   scores[activePlayer] += roundScore;
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  nextPlayer();
 });
 
 function nextPlayer() {
