9476e5bf8aaa209c6dacc8a17bed28344f9ef4e0
Allan Liebold <allan.liebold@gmail.com>
2018-03-31 21:17:16 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -5,21 +5,10 @@ roundScore = 0;
 activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
-
-/*
-function btn() {
-  This is a callback function.  
-}
-
-document.querySelector('.btn-roll').addEventListener('click', btn); 
-*/
-
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
-
-// anonymous function
 
 document.querySelector('.element').addEvenListener('click', function() {
   var dice = Math.floor(Math.random() * 6) + 1;
