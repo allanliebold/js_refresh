74318e4f89aab5846f1abb88c30d921dbccfe590
Allan Liebold <allan.liebold@gmail.com>
2018-04-04 06:00:05 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -26,8 +26,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-    
-    gamePlaying = false;
+    gamePlaying = false; // end the game
   } else {
     nextPlayer();
   }
