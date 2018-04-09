d58291287a8edbdf50f61b8dd8edabea21b3aba4
Allan Liebold <allan.liebold@gmail.com>
2018-04-09 06:05:28 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -77,6 +77,7 @@ function init() {
    // lastRoll = 0; 
   
    document.querySelector('.dice-1').style.display = 'none';
+   document.querySelector('.dice-2').style.display = 'none';
    document.getElementById('score-0').textContent = '0';
    document.getElementById('score-1').textContent = '0';
    document.getElementById('current-0').textContent = '0';
