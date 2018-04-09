9588b58a59e34d1ecf1014ed25b6a1bafa2034d5
Allan Liebold <allan.liebold@gmail.com>
2018-04-09 06:12:07 -0700

    Update objects.js

--- objects.js
+++ objects.js
@@ -61,3 +61,4 @@ console.log(steve.name + " " + steve.lastName + " is a " + steve.age + "-year-ol
 
 // Inheritance
 // An Athlete is also a Person, and so would inherit the properties of the Person constructor
+// Prototype property
