543b46cb97297c39f85ef0e3b6155b34fac9ff46
Allan Liebold <allan.liebold@gmail.com>
2018-04-16 06:12:10 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -96,3 +96,16 @@ interviewQuestion('anthropologist')('Gordon');
 
 // IIFE - Immediately Invoked Function Expression
 
+/*
+function game() {
+   var score = Math.random() * 10;
+   console.log(score >= 5);
+}
+game();
+*/
+
+(function () {
+   var score = Math.random() * 10;
+   console.log(score >= 5);
+})();
+
