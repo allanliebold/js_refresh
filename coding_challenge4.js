4c48aa4ef748d76cc7628af5544a3f98b1032fba
Allan Liebold <allan.liebold@gmail.com>
2018-04-17 06:07:54 -0700

    Update coding_challenge4.js

--- coding_challenge4.js
+++ coding_challenge4.js
@@ -4,5 +4,5 @@ function Question(question, answer, correct) {
   this.correct = correct;
 }
 
-var q1 = new Question('Are we having fun yet? 0 - Yes, 1 - No', ['Yes', 'No'], 0); // index 0 is the correct answer
+var q1 = new Question('Are we having fun yet?, ['Yes', 'No'], 0); // index 0 is the correct answer
 var q2 = 
