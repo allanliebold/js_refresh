d42dceb8cb48011f8c607b46d26425630091799b
Allan Liebold <allan.liebold@gmail.com>
2018-04-17 00:03:18 -0700

    Update objects.js

--- objects.js
+++ objects.js
@@ -133,3 +133,14 @@ function arrayCalc(arr, fn) {
   }
   return arrRes;
 };
+
+// Closures
+
+function retirement(retirementAge) {
+     var a = ' years left until retirement.';
+     return function(yearOfBirth) {
+          var age = 2018 - yearOfBirth;
+	  console.log((retirementAge - age) + a);
+     }
+}
+
