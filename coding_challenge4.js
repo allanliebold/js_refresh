afb7e86bb527ed822a2abf605f2e42bdfb7a5d7d
Allan Liebold <allan.liebold@gmail.com>
2018-04-17 23:16:23 -0700

    Update coding_challenge4.js

--- coding_challenge4.js
+++ coding_challenge4.js
@@ -17,3 +17,9 @@ var q2 = new Question('What is the answer?', ['This', 'That', 'Other'], 2);
 var q3 = new Question('This isn\'t not untrue.', ['True', 'False'], 1); 
 
 var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = prompt('Please select the correct answer.');
