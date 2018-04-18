1b01c0074f911ae1d2f9b0fb1072e66057474eaa
Allan Liebold <allan.liebold@gmail.com>
2018-04-17 17:52:58 -0700

    Update coding_challenge4.js

--- coding_challenge4.js
+++ coding_challenge4.js
@@ -4,6 +4,14 @@ function Question(question, answers, correct) {
   this.correct = correct;
 }
 
+Question.prototype.displayQuestion() {
+  console.log(this.question);
+  
+  for (var i = 0; i < this.answers.length; i++) {
+    console.log(i + ': ' + this.answers[i]);  
+  }
+}
+
 var q1 = new Question('Are we having fun yet?', ['Yes', 'No'], 0); // index 0 is the correct answer
 var q2 = new Question('What is the answer?', ['This', 'That', 'Other'], 2);
 var q3 = new Question('This isn\'t not untrue.', ['True', 'False'], 1); 
