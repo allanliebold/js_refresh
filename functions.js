a0c1a693f561a4243e1b172792d7c4f87f440b8f
Allan Liebold <allan.liebold@gmail.com>
2018-04-21 14:29:59 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -114,7 +114,7 @@ game();
 function sayHello(name) {
    var text = 'Hello, ' + name;
    return function() {
-      console.log(text);  
+      console.log(text);  // function called within a function keeps a reference to variables with the enclosing function
    }
 };
 
