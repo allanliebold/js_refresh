326771942184972692990800e6afafe3a32e7d2b
Allan Liebold <allan.liebold@gmail.com>
2018-04-22 11:45:09 -0700

    Update app.js

--- practice_project/budget_app/app.js
+++ practice_project/budget_app/app.js
@@ -9,16 +9,40 @@ var budgetController = (function() {
 // UI Controller
 var UIController = (function() {
   
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
   
-  
+  return {
+    getInput: function() {
+      return { // returns an object with the properties type, description, and value
+        type: document.querySelector(DOMstrings.inputType).value, // either 'inc' for income or 'exp' for expense  
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    }
+    
+    getDomStrings: function() {
+      return DOMstrings;
+    }
+  };  
 })();
 
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMstrings();
+  
   var ctrlAddItem = function() {
+    
     // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+    
     // 2. Add item to budget controller
     // 3. Add new item to UI
     // 4. Calculate budget
@@ -27,7 +51,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     console.log('Test');
   }
   
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
   
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) { // which is for older browsers
