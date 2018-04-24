b87d174a46ef0008f023467a76246a84065589bb
Allan Liebold <allan.liebold@gmail.com>
2018-04-24 05:48:57 -0700

    Update app.js

--- practice_project/budget_app/app.js
+++ practice_project/budget_app/app.js
@@ -60,6 +60,21 @@ var budgetController = (function() {
       return newItem;
     },
     
+    deleteItem: function(type, id) {
+      var ids, index;
+      
+      ids = data.allItems[type].map(function(current) {
+        return current.id; 
+      });
+      
+      index = ids.indexOf(id);
+      
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1); 
+      }
+      
+    },
+    
     calculateBudget: function() {
       calculateTotal('exp');
       calculateTotal('inc');
@@ -229,13 +244,24 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
   
   var ctrlDeleteItem = function(event) {
-    var itemID, splitID;
+    var itemID, splitID, type, ID;
     
     itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
     
     if (itemID) {
       
-      splidID = itemID.split('-');
+      splitID = itemID.split('-');
+      type = splitID[0];
+      ID = parseInt(splitID[1]);
+      
+      // 1. Delete item from data structure
+      budgetCtrl.deleteItem(type, ID);
+            
+      // 2. Remove item from UI
+            
+      
+      // 3. Update and show new budget
+            
       
     }
   };
