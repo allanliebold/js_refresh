6969d178c19d726ffba3ebdf5a4d78e2ebf760a6
Allan Liebold <allan.liebold@gmail.com>
2018-04-26 06:14:33 -0700

    Update app.js

--- practice_project/budget_app/app.js
+++ practice_project/budget_app/app.js
@@ -219,7 +219,19 @@ var UIController = (function() {
     },
     
     displayPercentages: function(percentages) {
+      var fields = document.querySelectorAll(DOMstrings.expensesPercentageLabel);  
+      
+      var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i); 
+        }
+      };
+     
+      nodeListForEach(fields, function(current, index) {
+      
         
+      });
+      
     },
     
     getDomStrings: function() {
