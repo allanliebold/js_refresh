25ae678348dd0ebad2c136c01ab650dcdd81bbbb
Allan Liebold <allan.liebold@gmail.com>
2018-04-28 22:42:52 -0700

    Update app.js

--- practice_project/budget_app/app.js
+++ practice_project/budget_app/app.js
@@ -242,13 +242,12 @@ var UIController = (function() {
       num = Math.abs(num).toFixed(2);
       
       numSplit = num.split('.');
-      int = numSplit[0];
-      dec = numSplit[1];
       
       if(int.length > 3) {
         int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);    
       }
       
+      dec = numSplit[1]
       type === 'exp' ? sign = '-' : sign = '+';
       return type + ' ' + int + dec;
     },
