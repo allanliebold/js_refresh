5c526f9ac61b1d03c8e95887a6dea4233ad25dd7
Allan Liebold <allan.liebold@gmail.com>
2018-04-29 08:43:28 -0700

    Update app.js

--- practice_project/budget_app/app.js
+++ practice_project/budget_app/app.js
@@ -220,7 +220,7 @@ var UIController = (function() {
     
     displayBudget: function(obj) {
       
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget);
       document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
       document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
       document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
