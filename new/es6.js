27a69c22e212285926f3711691a0314238316f47
Allan Liebold <allan.liebold@gmail.com>
2018-05-06 10:26:26 -0700

    Update es6.js

--- new/es6.js
+++ new/es6.js
@@ -104,4 +104,19 @@ ages6 = years.map((el, index) =>  {
 });
 console.log(ages6);
 
+// 'this' keyword with arrow functions
+
+//ES5
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    document.querySelector('.green').addEventListener('click', function() {
+      var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+      alert(str);
+    });
+  }
+}
+
+box5.clickMe();
 
