dfceafca484ad379caf3f418bf226d88b8c14630
Allan Liebold <allan.liebold@gmail.com>
2018-05-26 23:22:08 -0700

    Update Search.js

--- practice_project2/src/models/Search.js
+++ practice_project2/src/models/Search.js
@@ -5,6 +5,11 @@ export default class Search {
   getResults() {
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     const key = '462b1cc8d4f2730081462fbc65136320';
+    try {
+      
+    } catch (error) {
+      alert(error); 
+    }
   }
 }
 
