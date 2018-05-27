4b7cc37133e593257bf010b0790388d30162021d
Allan Liebold <allan.liebold@gmail.com>
2018-05-26 23:21:47 -0700

    Update Search.js

--- practice_project2/src/models/Search.js
+++ practice_project2/src/models/Search.js
@@ -2,6 +2,10 @@ export default class Search {
   constructor(query) {
     this.query = query; 
   }
+  getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '462b1cc8d4f2730081462fbc65136320';
+  }
 }
 
 import axios from 'axios';
