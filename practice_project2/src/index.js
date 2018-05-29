7222be15c9031ffa22ce543f3bdc0f30a261174a
Allan Liebold <allan.liebold@gmail.com>
2018-05-29 06:10:17 -0700

    Update index.js

--- practice_project2/src/index.js
+++ practice_project2/src/index.js
@@ -14,8 +14,16 @@ const controlSearch = async () => {
   const query = 'pizza';
   
   if (query) {
+    // 2 - New search object and add to state 
     state.search = new Search(query);
+    
+    // 3 - Prepare UI for results
+    
+    // 4 - Search for recipes
     await state.search.getResults();
+    
+    // 5 - Render results in UI
+    console.log(state.search.result);
   }
 }
 
