338fd4627ad71da9853471a59170197ed6bb3606
Allan Liebold <allan.liebold@gmail.com>
2018-05-30 06:11:48 -0700

    Update searchView.js

--- practice_project2/src/views/searchView.js
+++ practice_project2/src/views/searchView.js
@@ -6,7 +6,12 @@ const renderRecipe = recipe => {
   const markup = `
   <li>
     <a class="results__link results__link--active" href="#23456">
+      <figure class="results__fig">
+      
+      </figure>
+    <div class="results__data">
 
+    </div>
     </a>
   </li>
   `  
