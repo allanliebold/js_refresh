88446d68a029a227ac7f51e2e33a06f85e972055
Allan Liebold <allan.liebold@gmail.com>
2018-05-30 06:19:49 -0700

    Update base.js

--- practice_project2/src/views/base.js
+++ practice_project2/src/views/base.js
@@ -1,4 +1,5 @@
 export const elements = {
   search.document.querySelector('.search');
   searchInput: document.querySelector('.search__field');
+  searchResList: document.querySelector('.results__list');
 }
