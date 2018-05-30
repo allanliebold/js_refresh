58070b480530ad682525e79aaae38086cdef819a
Allan Liebold <allan.liebold@gmail.com>
2018-05-30 06:12:41 -0700

    Update searchView.js

--- practice_project2/src/views/searchView.js
+++ practice_project2/src/views/searchView.js
@@ -7,10 +7,11 @@ const renderRecipe = recipe => {
   <li>
     <a class="results__link results__link--active" href="#23456">
       <figure class="results__fig">
-      
+        <img src="img/test-1.jpg" alt="Test">
       </figure>
     <div class="results__data">
-
+      <h4 class="results__name">Pasta with Tomato ...</h4>
+      <p class="results__author">The Pioneer Woman</p>
     </div>
     </a>
   </li>
