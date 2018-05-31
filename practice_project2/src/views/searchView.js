951443fa6dacccd0380b3a31e847500d6a18bc98
Allan Liebold <allan.liebold@gmail.com>
2018-05-31 06:03:47 -0700

    Update searchView.js

--- practice_project2/src/views/searchView.js
+++ practice_project2/src/views/searchView.js
@@ -6,6 +6,10 @@ export const clearInput = () => {
   elements.searchInput.value = ''; 
 }
 
+export const clearResults = () => {
+  elements.searchResList.innerHTML = ''; 
+}
+
 const renderRecipe = recipe => {
   const markup = `
   <li>
