1ded49f5d96665bf88badbf915f3588daaae877a
Allan Liebold <allan.liebold@gmail.com>
2018-09-10 07:16:22 -0700

    Update compatibility.js

--- compatibility.js
+++ compatibility.js
@@ -2,3 +2,4 @@ var pasta = 'Spaghetti'; // ES5
 const meat = 'Pancetta'; // ES6
 let sauce = 'Eggs and cheese'; //ES6
 
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
