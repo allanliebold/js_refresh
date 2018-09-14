b5450c04cfa3df6c211552ad142949e9bea39b15
Allan Liebold <allan.liebold@gmail.com>
2018-09-14 06:20:12 -0700

    Update app.js

--- react/app.js
+++ react/app.js
@@ -2,3 +2,8 @@ const title = <h1 id="title">Introduction to React.js: Part I</h1>;
 
 const p1 = <p id="large">foo</p>;
 const p2 = <p id="small">bar</p>;
+const exampleJSXList = {
+  first: <li>First</li>,
+  second: <li>Second</li>,
+  third: <li>Third</li>
+};
