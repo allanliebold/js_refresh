825d5f661825ae9d2938201b75261e41f3cf12d6
Allan Liebold <allan.liebold@gmail.com>
2018-09-17 06:09:36 -0700

    Update PropsDisplayer.js

--- react/PropsDisplayer.js
+++ react/PropsDisplayer.js
@@ -4,5 +4,9 @@ import ReactDOM from 'react-dom';
 class PropsDisplayer extends React.Component {
   render() {
     const stringProps = JSON.stringify(this.props);
+    
+    return (
+      
+    );
   }
 }
