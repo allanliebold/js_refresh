0bd599bc6e69d3c1ab54d303183414907c9c3e74
Allan Liebold <allan.liebold@gmail.com>
2018-09-18 06:22:40 -0700

    Update Parent.js

--- react/Parent.js
+++ react/Parent.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 
 
 class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'Father' };
+  }
+  
   render() {
     return <div> </div>;
   }
