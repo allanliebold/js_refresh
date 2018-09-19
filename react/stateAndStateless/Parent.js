16ae529054da40f298854ffa37cc96cbe3f6f418
Allan Liebold <allan.liebold@gmail.com>
2018-09-19 06:27:37 -0700

    Update Parent.js

--- react/stateAndStateless/Parent.js
+++ react/stateAndStateless/Parent.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Child } from './Child';
 
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'Kid' };
+  }
+  
+  render() {
+    return (
+      <Child name={this.state.name} />
+    );
+  }
+};
