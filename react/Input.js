dcd9f88cba20fc6e3a4b2c0fd38554246c3799ba
Allan Liebold <allan.liebold@gmail.com>
2018-09-20 05:52:05 -0700

    Update Input.js

--- react/Input.js
+++ react/Input.js
@@ -5,6 +5,8 @@ class Input extends React.Component {
   constructor(props) {
     super(props);
     this.state = { userInput: '' };
+    
+    this.handleUserInput = this.handleUserInput.bind(this);
   }
   
   handleUserInput(e) {
