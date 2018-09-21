1e97dd7742ac585d90d134f46bba388698fca0d6
Allan Liebold <allan.liebold@gmail.com>
2018-09-21 06:15:20 -0700

    Update LifeCycle.js

--- react/LifeCycle.js
+++ react/LifeCycle.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class LifeCycle extends React.Component {
+  componentWillMount() {
+    
+  }
   
   render() {
     alert('Rendering.');
