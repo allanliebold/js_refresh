6b1a5def1f85979c21a513f78513f65bc5755577
Allan Liebold <allan.liebold@gmail.com>
2018-09-23 14:55:13 -0700

    Update example2.js

--- react/toDo/example2.js
+++ react/toDo/example2.js
@@ -7,6 +7,23 @@ function ToDoList(props) {
 }
 
 class AppComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this._nextToDoId = 1;
+        this.state = {
+            filter: { showCompleted: true },
+            todos: [
+                { id: this._nextToDoId++, text: "Hi", isCompleted: false },
+                { id: this._nextToDoId++, text: "Example", isCompleted: false },
+                { id: this._nextToDoId++, text: "Neat", isCompleted: true },
+                { id: this._nextToDoId++, text: "Done", isCompleted: true }
+            ]
+        };
+
+        this._onShowCompletedChanged = this._onShowCompletedChanged.bind(this);
+    }    
+    
   render() {
     
   }
