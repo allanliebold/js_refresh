5a66f68d587b8c198c84767b3e783eaa94c82dbc
Allan Liebold <allan.liebold@gmail.com>
2018-09-26 05:57:11 -0700

    Update example2.js

--- react/toDo/example2.js
+++ react/toDo/example2.js
@@ -4,7 +4,7 @@ function ToDoList({todos}) {
             {todos.map(todo =>
                 <li key={todo.id}>
                     <label>
-                        <input />
+                        <input type="checkbox" />
                         {todo.isCompleted
                             ? <del>{todo.text}</del>
                             : todo.text}
