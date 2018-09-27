a6ffe8c02f9604a37f2ad3251a2d52555a34bb02
Allan Liebold <allan.liebold@gmail.com>
2018-09-27 06:11:06 -0700

    Update example2.js

--- react/toDo/example2.js
+++ react/toDo/example2.js
@@ -53,7 +53,7 @@ class AppComponent extends React.Component {
     const {todos} = this.state;
     this.setState({
         todos: todos.map(oldTodo => {
-            
+            if(oldTodo.id !== todo.id)    
         });    
     });
   }
