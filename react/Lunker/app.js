dae164613c857afdd9460cb67ff0b9fb11bb2431
Allan Liebold <allan.liebold@gmail.com>
2018-09-30 10:47:02 -0700

    Update app.js

--- react/Lunker/app.js
+++ react/Lunker/app.js
@@ -1,6 +1,15 @@
 class Game extends React.Component {
   render() {
-    
+            return(
+            <div>
+                <h2>{currScene.name}</h2>
+                <p>{currScene.text}</p>
+                {northButton}<br />
+                {westButton}
+                {eastButton}<br />
+                {southButton}
+             </div>
+        );
   }
 };
 
