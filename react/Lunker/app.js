e1c9bf713f54ad663ad92433c733d919c04baed7
Allan Liebold <allan.liebold@gmail.com>
2018-09-30 11:02:26 -0700

    Update app.js

--- react/Lunker/app.js
+++ react/Lunker/app.js
@@ -13,32 +13,31 @@ class Game extends React.Component {
       const {currScene} = this.state;
       let northButton, southButton, eastButton, westButton;  
 
-  currScene.go[0]
-    ? northButton = <button onClick={() => this._changeScene(currScene.go[0]) }>N</button>
-    : northButton = <button>X</button>;
+      currScene.go[0]
+         ? northButton = <button onClick={() => this._changeScene(currScene.go[0]) }>N</button>
+         : northButton = <button>X</button>;
 
-  currScene.go[1]
-    ? southButton = <button onClick={() => this._changeScene(currScene.go[1]) }>S</button>
-    : southButton = <button>X</button>;
+      currScene.go[1]
+         ? southButton = <button onClick={() => this._changeScene(currScene.go[1]) }>S</button>
+         : southButton = <button>X</button>;
 
-  currScene.go[2]
-    ? eastButton = <button onClick={() => this._changeScene(currScene.go[2]) }>E</button>
-    : eastButton = <button>X</button>;
+      currScene.go[2]
+         ? eastButton = <button onClick={() => this._changeScene(currScene.go[2]) }>E</button>
+         : eastButton = <button>X</button>;
 
-  currScene.go[3]
-    ? westButton = <button onClick={() => this._changeScene(currScene.go[3]) }>W</button>
-    : westButton = <button>X</button>;
-
-  return(
-      <div>
-        <h2>{currScene.name}</h2>
-        <p>{currScene.text}</p>
-        {northButton}<br />
-        {westButton}
-        {eastButton}<br />
-        {southButton}
-      </div>
-    );
+      currScene.go[3]
+         ? westButton = <button onClick={() => this._changeScene(currScene.go[3]) }>W</button>
+         : westButton = <button>X</button>;
+      return(
+         <div>
+            <h2>{currScene.name}</h2>
+            <p>{currScene.text}</p>
+            {northButton}<br />
+            {westButton}
+            {eastButton}<br />
+            {southButton}
+         </div>
+      );
   }
 };
 
