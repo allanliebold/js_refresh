7642221bb7b5c74742c1cf429dac0ac41908f457
Allan Liebold <allan.liebold@gmail.com>
2018-10-04 06:24:58 -0700

    Update app.js

--- react/Lunker/app.js
+++ react/Lunker/app.js
@@ -2,7 +2,7 @@ const scenesArr = [
   {
     id: 1,
     name: 'Room',
-    text: 'There is a passage to the north.',
+    text: 'There is a passage to the north. (...and also one to the west.)',
     go: ['1', false, false, '2'],
     visited: false
   },
