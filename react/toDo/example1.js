8edae3dc3deda23e65535ed3b47012ad5e0a3a40
Allan Liebold <allan.liebold@gmail.com>
2018-10-07 09:21:16 -0700

    Update example1.js

--- react/toDo/example1.js
+++ react/toDo/example1.js
@@ -1,2 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 5,
+      title: "Count"
+    };
+    
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+    
+  render() {
+    const {count, title} = this.state;
+    
+    return (
+      <section className="site-wrap">
+        <h2>{title}</h2>
+        <p>{count}</p>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}>+</button>
+      </section>
+    );
+  }
+  
+  increment() {
+    const {count} = this.state;
+    this.setState({ count: count + 1 });
+  }
+  
+  decrement() {
+    const {count} = this.state;
+    this.setState();
+  }
+}
+
+ReactDOM.render(
+    <AppComponent />,
+    document.getElementById('application')
+);
