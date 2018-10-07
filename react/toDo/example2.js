c88cbc4f08cb3daec6504dc5bd8cda5c34546123
Allan Liebold <allan.liebold@gmail.com>
2018-10-06 21:29:45 -0700

    Update example2.js

--- react/toDo/example2.js
+++ react/toDo/example2.js
@@ -14,7 +14,7 @@ class AppComponent extends React.Component {
       <section className="site-wrap">
         <h2>{title}</h2>
         <p>{count}</p>
-        <button>-</button>
+        <button onClick={this.decrement}>-</button>
         <button>+</button>
       </section>
     );
