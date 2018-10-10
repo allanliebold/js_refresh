c566272ccc419df3a4b1f0d93d778f4e5f386e19
Allan Liebold <allan.liebold@gmail.com>
2018-10-10 09:35:36 -0700

    Update app2.js

--- sqlite/app2.js
+++ sqlite/app2.js
@@ -1,1 +1,4 @@
+const { calculateAverages, addClimateRowToObject, logNodeError, printQueryResults } = require('./utils');
+const sqlite = require('sqlite3');
 
+const db = new sqlite.Database('./db.sqlite');
