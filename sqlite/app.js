99839ebe862abdbd310c4c74e951572bf0836a8f
Allan Liebold <allan.liebold@gmail.com>
2018-10-10 06:04:19 -0700

    Update app.js

--- sqlite/app.js
+++ sqlite/app.js
@@ -26,4 +26,6 @@ ids.forEach(id => {
 db.run("INSERT INTO Temperature Data(location, year) VALUES($location, $year)", {
   $location: newRow.location,
   $year: newRow.year
-}, function(error) {});
+}, function(error) {
+  console.log(this.lastID);
+});
