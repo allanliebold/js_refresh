32766b9217c070b10c981b8eddf5cfe46dce0ef1
Allan Liebold <allan.liebold@gmail.com>
2018-10-10 05:59:53 -0700

    Update app.js

--- sqlite/app.js
+++ sqlite/app.js
@@ -3,7 +3,9 @@ const sqlite3 = require('sqlite3');
 
 const ids = [5, 20, 43, 100];
 const db = new sqlite.Database('./db.sqlite');
-const newRow = {};
+const newRow = {
+  location: 'London, England'
+};
 
 db.all("SELECT * FROM TemperatureData WHERE year = 1970" (error, rows) => {
   printQueryResults(rows);
