0c1867120d5cc6f6dd2a2a5c127f9b887a01d7c8
Allan Liebold <allan.liebold@gmail.com>
2018-10-12 06:05:57 -0700

    Update exercises.js

--- sqlite/exercises.js
+++ sqlite/exercises.js
@@ -1,3 +1,7 @@
 const db = require('./db');
 
-db.each('SELECT * FROM Flower', (error, row) => {});
+db.each('SELECT * FROM Flower', (error, row) => {
+  if(row.petal_color === 'blue') {
+    
+  }
+});
