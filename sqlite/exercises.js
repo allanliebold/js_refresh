bc3cd344161e24a958c25ae1b652a59a18021358
Allan Liebold <allan.liebold@gmail.com>
2018-10-15 06:26:00 -0700

    Update exercises.js

--- sqlite/exercises.js
+++ sqlite/exercises.js
@@ -1,6 +1,6 @@
-// Select all rows from the Flower database and log the ones with petal_color blue. 
 const db = require('./db');
 
+// Select all rows from the Flower database and log the ones with petal_color blue. 
 db.each('SELECT * FROM Flower', (error, row) => {
   if(row.petal_color === 'blue') {
     console.log(row);
