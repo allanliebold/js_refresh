153c5656ff3c1103f9edb18c0b939c70df39a957
Allan Liebold <allan.liebold@gmail.com>
2018-10-21 11:35:04 -0500

    Update main.js

--- jquery/main.js
+++ jquery/main.js
@@ -16,6 +16,7 @@ $(document).ready(() => {
     $('.first-image').slideToggle();
   });
   
+  // Mouse enter
   $('.product-photo').on('mouseenter', event => {
     $(event.currentTarget).addClass('photo-active')
   }).on('mouseleave', event => {
