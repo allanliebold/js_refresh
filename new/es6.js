f8b49c711ff173a85dc615bae146ec6058cf96c1
Allan Liebold <allan.liebold@gmail.com>
2018-10-22 06:42:26 -0700

    Update es6.js

--- new/es6.js
+++ new/es6.js
@@ -9,7 +9,7 @@ console.log(name5);
 //ES6 - let and const
 const name6 = 'Jane Smith';
 let age6 = 27;
-name6 = 'Jane Miller'; // error: Assignment to constant variable. const is immutable.
+name6 = 'Jane Miller'; // error: Assignment to constant variable. const is immutable (well, kind of. More later.)
 console.log(name6);
 
 // var is function-scoped. let and const are block-scoped
