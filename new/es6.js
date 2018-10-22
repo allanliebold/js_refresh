01b9b9cd69bc7befa882942807a1fad205d27236
Allan Liebold <allan.liebold@gmail.com>
2018-10-22 06:38:07 -0700

    Update es6.js

--- new/es6.js
+++ new/es6.js
@@ -6,7 +6,7 @@ var age5 = 27;
 name5 = 'Jane Miller';
 console.log(name5);
 
-//ES6
+//ES6 - let and const
 const name6 = 'Jane Smith';
 let age6 = 27;
 name6 = 'Jane Miller'; // error: Assignment to constant variable. const is immutable.
