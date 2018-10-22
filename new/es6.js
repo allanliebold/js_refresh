65a53b3912da4c5abad68537687bd1bc74c80fb4
Allan Liebold <allan.liebold@gmail.com>
2018-10-22 06:44:37 -0700

    Update es6.js

--- new/es6.js
+++ new/es6.js
@@ -31,7 +31,8 @@ if(passedTest) {
 }
 
 This works because of where firstName was declared. Can't do the same for const since it's immutable.
-Note - const isn't actually immutable. The binding of const is immutable but the values contained can be reassigned.
+Note - const is actually not completely immutable. The binding of const is immutable but the values contained can be 
+reassigned.
 For example, if you declare an array as a const, the values of the indices can be changed. 
 */
 
