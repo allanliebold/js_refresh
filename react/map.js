4ad6f521f271bcc7ad212c9793bf104c36ad98ee
Allan Liebold <allan.liebold@gmail.com>
2018-11-05 06:05:36 -0800

    Update map.js

--- react/map.js
+++ react/map.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const numbers = [1, 2, 3, 4];
-const people = ['Bill', 'Bob', 'Dave'];
+const numbers = [1, 2, 3, 4, 5];
+const people = ['Bill', 'Bob', 'Dave', 'Doug'];
 
 const peopleList = people.map((person, i) =>
   <li key={'person_' + i}>{person}</li>
