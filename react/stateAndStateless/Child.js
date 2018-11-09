68d18e1d421047c24c412b84fb8f9d330f3b6559
Allan Liebold <allan.liebold@gmail.com>
2018-11-09 06:18:33 -0800

    Update Child.js

--- react/stateAndStateless/Child.js
+++ react/stateAndStateless/Child.js
@@ -4,6 +4,6 @@ import React from 'react';
 
 export class Child extends React.Component {
   render() {
-    return <h1>Hi, I'm {this.props.name}!</h1>; 
+    return <h1>Hi, I am {this.props.name}!</h1>; 
   }
 };
