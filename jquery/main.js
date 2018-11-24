24919056250bece37ad1063891013b3469a8adbc
Allan Liebold <allan.liebold@gmail.com>
2018-11-24 14:04:42 -0800

    Update main.js

--- jquery/main.js
+++ jquery/main.js
@@ -21,6 +21,7 @@ $(document).ready(() => {
     $(event.currentTarget).removeClass('photo-active')
   });
   
+  //
   $('.menu-button').on('mouseenter', () => {
   $('.nav-menu').removeClass('hide');
   $('.menu-button').addClass('button-active')
