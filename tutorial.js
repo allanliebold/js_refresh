06f01e46e60704050c0d438781472ce259d001d4
Allan Liebold <allan.liebold@gmail.com>
2018-12-28 09:53:16 -0800

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -140,6 +140,8 @@ function golfScore(par, strokes) {
     return names[0];
   } else if (strokes <= par - 2) {
     return names[1];
+  } else if (strokes == par - 1) {
+    return names[2];
   } 
 }
 
