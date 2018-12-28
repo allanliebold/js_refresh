a9cb327b768e269f3cdd5235ad18713a6c93ad44
Allan Liebold <allan.liebold@gmail.com>
2018-12-28 09:52:54 -0800

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -138,7 +138,9 @@ var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", 
 function golfScore(par, strokes) {
   if(strokes == 1) {
     return names[0];
-  }
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } 
 }
 
 golfScore(5, 4);
