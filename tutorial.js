32ffe5d95549cd1bf5b38e0869fcbbe4f47043ae
Allan Liebold <allan.liebold@gmail.com>
2018-12-28 09:39:05 -0800

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -136,11 +136,8 @@ andOperator(true, false); // Half right.
 // Golf
 var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 function golfScore(par, strokes) {
-  // Only change code below this line
-  
   
   return "Change Me";
-  // Only change code above this line
 }
 
 golfScore(5, 4);
