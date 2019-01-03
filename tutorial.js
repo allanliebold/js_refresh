f836f85d5ad8a6fa645da226319f4d66572f206d
Allan Liebold <allan.liebold@gmail.com>
2019-01-03 05:35:24 -0800

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -214,8 +214,7 @@ function cc(card) {
     case 2: case 3: case 4: case 5: case 6:
       count++;
       break;
-    case 7:
-    case 8:
+    case 7: case 8:
     case 9:
       break;
     case 10:
