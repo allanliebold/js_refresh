7ee37dca8feb679a48b79b535cc8174500249ac1
Allan Liebold <allan.liebold@gmail.com>
2019-01-04 09:07:29 -0800

    Update record_collection.js

--- record_collection.js
+++ record_collection.js
@@ -26,12 +26,22 @@ var collection = {
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 function updateRecords(id, prop, value) {
+  if(value == "") {
+    delete collection[id][prop];
+    return collection
+  }
+
   if(prop == "tracks") {
     if(collection[id].hasOwnProperty("tracks") == false) {
       collection[id]["tracks"] = [];
     }
-    collection[id]["tracks"].push(value);
-  }  
+    if(value != "") {
+      collection[id]["tracks"].push(value);
+      return collection;
+    }
+  }
+  
+  collection[id][prop] = value;
   return collection;
 }
 
