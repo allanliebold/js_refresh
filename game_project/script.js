7058e83ce5d730030b8ccce8ca66a252899d4bd2
Allan Liebold <allan.liebold@gmail.com>
2019-01-08 01:56:20 -0800

    Update script.js

--- game_project/script.js
+++ game_project/script.js
@@ -1,10 +1,10 @@
 // Game Project Prototype
 
 // Room object constructor
-function Room(name, desc, exits) {
+function Room(name, desc, exits = []) {
   this.name = name;
   this.desc = desc;
-  this.exits = [];
+  this.exits = exits;
 }
 
 function Map(rooms) {
