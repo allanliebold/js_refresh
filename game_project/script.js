d5589c825a9d26528322395e43b1b6790f2ee8be
Allan Liebold <allan.liebold@gmail.com>
2019-01-08 01:57:27 -0800

    Update script.js

--- game_project/script.js
+++ game_project/script.js
@@ -18,5 +18,4 @@ function connectRooms(start, end, connection) {
 
 var firstRoom = Room('First Room', 'This is the first room', ['North': secondRoom]);
 var secondRoom = Room('Second Room', 'This is the second room');
-connectRooms(firstRoom, secondRoom, 'North');
 connectRooms(secondRoom, firstRoom, 'South');
