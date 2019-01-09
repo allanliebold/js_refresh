f31e759cca523f00903e33d4aa9df38c00c5265a
Allan Liebold <allan.liebold@gmail.com>
2019-01-09 09:26:06 -0800

    Update tutorial_es6.js

--- tutorial_es6.js
+++ tutorial_es6.js
@@ -4,7 +4,7 @@
 const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 const squareList = (arr) => {
   "use strict";
-  const filteredArr = realNumberArray.filter((num) => num >= 0 && Number.isInteger(num)).map((num) => num * num);
+  return realNumberArray.filter((num) => num >= 0 && Number.isInteger(num)).map((num) => num * num);
 };
 
 const squaredIntegers = squareList(realNumberArray);
