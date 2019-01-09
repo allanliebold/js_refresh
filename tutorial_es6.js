0977bd57b6afde4f92feddf8a8625d61fbd15799
Allan Liebold <allan.liebold@gmail.com>
2019-01-09 09:07:14 -0800

    Update tutorial_es6.js

--- tutorial_es6.js
+++ tutorial_es6.js
@@ -1,1 +1,12 @@
 // ES6 Tutorial
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+
+  const squaredIntegers = arr;
+  return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
