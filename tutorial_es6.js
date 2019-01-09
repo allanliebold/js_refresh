b3fd04a1030db43d50b10c1150b1dced2aaabd52
Allan Liebold <allan.liebold@gmail.com>
2019-01-09 11:10:58 -0800

    Update tutorial_es6.js

--- tutorial_es6.js
+++ tutorial_es6.js
@@ -5,7 +5,8 @@ const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 const squareList = (arr) => {
   "use strict";
   // Filter realNumberArray to return only positive integers. Then use map to return a new array with these integers squared.
-  return realNumberArray.filter((num) => num >= 0 && Number.isInteger(num)).map((num) => num * num);
+  return realNumberArray.filter((num) => num >= 0 && Number.isInteger(num))
+                        .map((num) => num * num);
 };
 
 // Declare a const called squareIntegers to store the return value of squareList, with realNumberArray passed as its argument.
