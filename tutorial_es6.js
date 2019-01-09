e4bfef4a198c38ae6730452d147465a0f3f32230
Allan Liebold <allan.liebold@gmail.com>
2019-01-09 14:29:49 -0800

    Update tutorial_es6.js

--- tutorial_es6.js
+++ tutorial_es6.js
@@ -16,9 +16,8 @@ console.log(squaredIntegers); // 16, 1764, 36
 // Rest Operator
 const sum = (function() {
   "use strict";
-  return function sum(x, y, z) {
-    const args = [ x, y, z ];
+  return function sum(...args) {
     return args.reduce((a, b) => a + b, 0);
   };
 })();
-console.log(sum(1, 2, 3)); // 6
+console.log(sum(2, 2, 5)); // 9
