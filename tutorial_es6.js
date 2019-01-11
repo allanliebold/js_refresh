ad5dd263a53b8e703e855f8de4a1c58efcd4340e
Allan Liebold <allan.liebold@gmail.com>
2019-01-11 05:57:47 -0800

    Update tutorial_es6.js

--- tutorial_es6.js
+++ tutorial_es6.js
@@ -53,6 +53,7 @@ var maximus = Math.max.apply(null, arr); // 89
 // The spread operator will separate each index of the array to pass as individual arguments.
 const arr = [6, 89, 3, 45];
 const maximus = Math.max(...arr); // 89
+const minimus = Math.min(...arr);
 
 const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
 let arr2;
