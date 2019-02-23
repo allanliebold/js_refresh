3e89a66f2f1eeb72717e46f9a725da41235a9de6
Allan Liebold <allan.liebold@gmail.com>
2019-02-23 13:37:26 -0800

    Update tutorial.js

--- react/tutorial.js
+++ react/tutorial.js
@@ -321,3 +321,24 @@ class Results extends React.Component {
     )
   }
 }
+
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '' 
+    };
+    
+  this.handleChange = this.handleChange.bind(this);
+    
+  handleChange(event) {
+    this.setState({ input: event.target.value }) 
+  }
+    
+  render() {
+    
+    return(
+        
+    );
+  }
+}
