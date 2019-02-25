8bb6341ab541420973a6e2abdb5cb76e5051a5ae
Allan Liebold <allan.liebold@gmail.com>
2019-02-25 05:56:07 -0800

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -3,5 +3,10 @@
 cons convertToF = (celsius) => (celsius * 9/5) + 32;
 
 function reverseString(str) {
-  
+  let strArr = str.split('');
+  let reverseArr = [];
+  for(let i = strArr.length; i > -1; i--) {
+    reverseArr.push(strArr[i]); 
+  }
+  return reverseArr.join('');
 }
