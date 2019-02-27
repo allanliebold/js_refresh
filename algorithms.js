91944799989ae7f6018eb402c703a53a9ba0f454
Allan Liebold <allan.liebold@gmail.com>
2019-02-27 05:59:30 -0800

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -56,6 +56,6 @@ function largestofFour(arr) {
 largestOfFour([[4,5,1,3], [13,27,18,26],[32,35,37,39],[1000,1001,857,1]]);
 
 function confirmEnding(str, target) {
-  
+  return str.slice(target.length - (target.length * 2)) == target;
 }
 
