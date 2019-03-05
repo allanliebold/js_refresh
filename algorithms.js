95b5eec4c90a6c0997a1459bc52aaf3f60e06216
Allan Liebold <allan.liebold@gmail.com>
2019-03-05 00:13:03 -0800

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -82,6 +82,11 @@ truncateString("abracadabra", 4); // abra...
 function sumAll(arr) {
   var max = Math.max(arr[0], arr[1]);
   var min = Math.min(arr[0], arr[1]);
+  var sum = 0;
+  for (var i = min; i <= max; i++) {
+    sum += i; 
+  }
+  return sum; 
 }
 
 //Advanced
