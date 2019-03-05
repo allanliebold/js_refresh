0e002d7377414b745edeabd58632d176260a2d64
Allan Liebold <allan.liebold@gmail.com>
2019-03-05 00:21:50 -0800

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -112,9 +112,15 @@ function diffArray(arr1, arr2) {
   
   function compareArr(first, second) {
     for(let i = 0; i < first.length; i++) {
-      
+      if(!second.includes(first[i]) {
+        newArr.push(first[i]);   
+      }
     }
   }
+
+  compareArr(arr1, arr2);
+  compareArr(arr2, arr1);
+
   return newArr;
 }
 
