446b5f5b48ab45ec3f30aa6f099aab375280e5e7
Allan Liebold <allan.liebold@gmail.com>
2019-03-08 05:57:05 -0800

    Update regex.js

--- regex.js
+++ regex.js
@@ -1,6 +1,8 @@
 let regexp = /t.[a-z]*t/;
 let regexp = /E+/;
 
+let crowd = 'P1P2P3P4P5P6CCCP7P8P9';
+
 "First Second".replace(/(\w+)\s(\w+)/, '$2 $1'); // "Second First" 
 
 let originalText = "I am good.";
