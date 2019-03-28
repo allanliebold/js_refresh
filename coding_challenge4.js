418a08072333fa86b866b7113103eb2464d2b384
Allan Liebold <allan.liebold@gmail.com>
2019-03-28 05:59:41 -0700

    Update coding_challenge4.js

--- coding_challenge4.js
+++ coding_challenge4.js
@@ -18,7 +18,7 @@ var q2 = new Question('What is the answer?', ['This', 'That', 'Other'], 2);
 var q3 = new Question('This isn\'t not untrue.', ['True', 'False'], 1); // I think this is right?
 var q4 = new Question('Is that it?', ['Yup', 'Nope'], 0);
 
-var questions = [q1, q2, q3];
+var questions = [q1, q2, q3, q4];
 
 var n = Math.floor(Math.random() * questions.length);
 
