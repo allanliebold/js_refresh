97af92d32272c3aa87e9fe17f378716179b0b841
Allan Liebold <allan.liebold@gmail.com>
2019-03-27 23:22:44 -0700

    Update coding_challenge4.js

--- coding_challenge4.js
+++ coding_challenge4.js
@@ -16,7 +16,7 @@ Question.prototype.displayQuestion = function() {
 var q1 = new Question('Are we having fun yet?', ['Yes', 'No'], 0); // index 0 is the correct answer
 var q2 = new Question('What is the answer?', ['This', 'That', 'Other'], 2);
 var q3 = new Question('This isn\'t not untrue.', ['True', 'False'], 1); 
-var q4 = new Question('Is that it?');
+var q4 = new Question('Is that it?', ['Yup', 'Nope']);
 
 var questions = [q1, q2, q3];
 
