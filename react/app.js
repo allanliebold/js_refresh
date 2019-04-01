9ab2b157b0e1951636a097d3cf41b3b4b758fd49
Allan Liebold <allan.liebold@gmail.com>
2019-03-31 17:56:22 -0700

    Update app.js

--- react/app.js
+++ react/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 const title = <h1 id="title">Introduction to React.js: Part I</h1>;
 const myArticle = <article></article>;
-
 const exampleJSXList = {
   first: <li>First</li>,
   second: <li>Second</li>,
