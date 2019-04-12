de3e0060f1d560b1e0107e5af3a8fe88a81ae7f0
Allan Liebold <allan.liebold@gmail.com>
2019-04-12 06:07:47 -0700

    Update data_structures_tutorial.js

--- data_structures_tutorial.js
+++ data_structures_tutorial.js
@@ -14,6 +14,7 @@ console.log(firstArr); // first, second, third
 console.log(shifted); // zero
 
 let spliceArr = ['This', 'array', 'has', 'some', 'extra', 'words'];
+
 let removedWord = spliceArr.splice(4, 1);
 
 console.log(spliceArr); // This, array, has, some, words
