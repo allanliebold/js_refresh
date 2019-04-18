6abbdd3528d4323fd36016dd5287df0ffd9219e3
Allan Liebold <allan.liebold@gmail.com>
2019-04-18 06:06:17 -0700

    Update codingChallenge5.js

--- new/codingChallenge5.js
+++ new/codingChallenge5.js
@@ -28,6 +28,7 @@ class Street extends Element {
   
   classifyStreet() {
     const classification = new Map();
+    
     classification.set(1, 'tiny');
     classification.set(2, 'small');
     classification.set(3, 'normal');
