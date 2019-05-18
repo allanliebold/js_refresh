fe2f59d033447d587ecfa966862e439a84eedfb3
Allan Liebold <allan.liebold@gmail.com>
2019-05-17 22:50:06 -0700

    Update compatibility.js

--- compatibility.js
+++ compatibility.js
@@ -7,6 +7,7 @@ let sauce = 'Eggs and cheese'; //ES6
 const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
 
 let esFivePercentageSupport = 95.88; // 95.88% of browsers support ES5, according to caniuse.com
+
 let esSixTemplateLiterals = 88.05 // 88.05% of browsers support ES6 template literals
 
 // Babel transpiles ES6 into ES5
