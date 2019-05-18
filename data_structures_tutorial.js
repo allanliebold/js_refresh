bfdbcdead0b4a3f60864b89848b2d246da8ff82e
Allan Liebold <allan.liebold@gmail.com>
2019-05-17 22:49:49 -0700

    Update data_structures_tutorial.js

--- data_structures_tutorial.js
+++ data_structures_tutorial.js
@@ -2,8 +2,7 @@
 let firstArr = ['first', 'second', 'third'];
 firstArr.unshift('zero');
 firstArr.push('fourth');
-console.log(firstArr); 
-// zero, first, second, third, fourth
+console.log(firstArr); // zero, first, second, third, fourth
 
 firstArr.pop();
 console.log(firstArr); // zero, first, second, third
