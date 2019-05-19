7735853d558030b34222979c94374fb36087ca54
Allan Liebold <allan.liebold@gmail.com>
2019-05-18 23:07:36 -0700

    Update letAndconst.js

--- new/letAndconst.js
+++ new/letAndconst.js
@@ -10,7 +10,6 @@ console.log(name5);
 const name6 = 'Jane Smith';
 let age6 = 27;
 name6 = 'Jane Miller'; // error: Assignment to constant variable. const is immutable.
-
 console.log(name6);
 
 // var is function-scoped. let and const are block-scoped
