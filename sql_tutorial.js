7f94a6bd18cb370d113008e22c94d1655403bf93
Allan Liebold <allan.liebold@gmail.com>
2019-05-24 05:53:44 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -1,39 +1,4 @@
-// SQL Database
-create database Customer
-use Customer
 
-create table Customer
-(
-  FirstName varchar(50),
-  LastName varchar(50),
-  Age int
-)
-
-insert into Customer 
-(FirstName, LastName, Age)
-  values('John', 'Doe', 40)
-
-insert into Customer 
-(FirstName, LastName, Age)
-  values('Bob', 'Roberts', 60)
-
-insert into Customer 
-(FirstName, LastName, Age)
-  values('Sally', 'Doe', 38)
-
-insert into Customer 
-(FirstName, LastName, Age)
-  values('Jane', 'Smith', 40)
-
-select FirstName from Customer;
-
-/*
-FirstName
-John
-Bob
-Sally 
-Jane
-*/
 
 select FirstName,LastName 
 from Customer where Age=40;
