3604156c02e67b02ce4ca31ba5e959dbc5f1b305
Allan Liebold <allan.liebold@gmail.com>
2019-05-24 05:55:49 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -52,7 +52,6 @@ INSERT INTO store VALUES (13, "Shoes", 5, 10, 4);
 INSERT INTO store VALUES (14, "Shoes", 5, 10, 4);
 INSERT INTO store VALUES (15, "Shoes", 5, 10, 4);
 
-
 CREATE TABLE exercise_log
   (id INTEGER PRIMARY KEY AUTOINCREMENT,
    type TEXT,
