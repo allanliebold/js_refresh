3cc0e45b00b9a6c769e457c8ae85a62e5d42bb44
Allan Liebold <allan.liebold@gmail.com>
2019-05-24 06:16:40 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -116,3 +116,4 @@ INSERT INTO songs (artist, title)
 SELECT title FROM songs WHERE artist = "Queen";
 SELECT artist FROM artists WHERE genre = "Pop";
 SELECT title FROM songs WHERE artist IN (SELECT name FROM artists WHERE genre = "Pop");
+SELECT type FROM exercise_log GROUP BY type HAVING COUNT(*) >= 2;
