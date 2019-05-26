50e192e8d3cda7433ac0e16a52202bd074307062
Allan Liebold <allan.liebold@gmail.com>
2019-05-25 20:22:22 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -129,3 +129,11 @@ INSERT INTO students (first_name, last_name, email, phone, birthdate)
     VALUES ("Peter", "Rabbit", "peter@rabbit.com", "555-6666", "2002-06-24");
 INSERT INTO students (first_name, last_name, email, phone, birthdate)
     VALUES ("Alice", "Wonderland", "alice@wonderland.com", "555-4444", "2002-07-04");
+    
+CREATE TABLE student_grades (id INTEGER PRIMARY KEY,
+    student_id INTEGER,
+    test TEXT,
+    grade INTEGER);
+
+INSERT INTO student_grades (student_id, test, grade)
+    VALUES (1, "Nutrition", 95);
