18cb9a79a6e7cb68c750e5dad62d94181ef996a1
Allan Liebold <allan.liebold@gmail.com>
2019-05-26 23:07:28 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -118,3 +118,4 @@ INSERT INTO student_grades (student_id, test, grade)
     VALUES (1, "Chemistry", 85);
 INSERT INTO student_grades (student_id, test, grade)
     VALUES (2, "Chemistry", 95);
+INSERT INTO student_graders ()
