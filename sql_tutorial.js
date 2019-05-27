ab626ba32d77f645dfe4d8d362744e3e8625573d
Allan Liebold <allan.liebold@gmail.com>
2019-05-26 23:04:54 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -46,7 +46,7 @@ INSERT INTO exercise_log(type, minutes, calories, heart_rate) VALUES ("jogging",
 INSERT INTO exercise_log(type, minutes, calories, heart_rate) VALUES ("walking", 20, 40, 90);
 INSERT INTO exercise_log(type, minutes, calories, heart_rate) VALUES ("biking", 15, 100, 150);
 INSERT INTO exercise_log(type, minutes, calories, heart_rate) VALUES ("weights", 10, 50, 95);
-INSERT INTO exercise_log(type, minutes, calories, heart_rate) VALUES ("aerobics", 25);
+INSERT INTO exercise_log(type, minutes, calories, heart_rate) VALUES ("aerobics", 25, 70, 110);
 
 SELECT * FROM exercise_log WHERE calories > 50 AND minutes < 30;
 SELECT type FROM exercise_log WHERE minutes > 10; 
