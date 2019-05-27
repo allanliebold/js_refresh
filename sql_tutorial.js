86844f8334511b9d8c269c27a4ea450047c9a798
Allan Liebold <allan.liebold@gmail.com>
2019-05-26 23:05:36 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -33,6 +33,7 @@ INSERT INTO todo_list VALUES (1, "Laundry", 90);
 INSERT INTO todo_list VALUES (2, "Dishes", 20);
 INSERT INTO todo_list VALUES (3, "Study", 30);
 INSERT INTO todo_list VALUES (4, "Nap", 60);
+INSERT INTO todo_list VALUES ();
 
 SELECT SUM(minutes) FROM todo_list;
 
