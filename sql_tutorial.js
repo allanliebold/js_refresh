81e1277b26c006db7316d3e3e73f4cd25c2d6e63
Allan Liebold <allan.liebold@gmail.com>
2019-05-26 23:06:52 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -99,8 +99,7 @@ CREATE TABLE students (id INTEGER PRIMARY KEY,
     phone TEXT,
     birthdate TEXT);
 
-//
-
+// Student grades
 INSERT INTO students (first_name, last_name, email, phone, birthdate)
     VALUES ("Peter", "Rabbit", "peter@rabbit.com", "555-6666", "2002-06-24");
 INSERT INTO students (first_name, last_name, email, phone, birthdate)
