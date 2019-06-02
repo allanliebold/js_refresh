6f2735d735fc674e4528849302ac389578a4ce05
Allan Liebold <allan.liebold@gmail.com>
2019-06-01 17:07:37 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -7,7 +7,7 @@ INSERT INTO groceries VALUES (3, "Tea", 2, 3);
 INSERT INTO groceries VALUES (4, "Bread", 1, 4);
 INSERT INTO groceries VALUES (5, "Bananas", 4, 1);
 INSERT INTO groceries VALUES (6, "Cereal", 2, 3);
-INSERT INTO groceries VALUES (7, "Eggs", 6);
+INSERT INTO groceries VALUES (7, "Eggs", 6, 6);
 
 SELECT * FROM groceries WHERE quantity > 1 ORDER BY aisle;
 
