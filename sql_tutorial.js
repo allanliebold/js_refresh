0c4c8cc3fe0cff8cb0437881c76e16b729d7f5eb
Allan Liebold <allan.liebold@gmail.com>
2019-06-03 05:55:44 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -3,6 +3,8 @@ CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, ais
 
 INSERT INTO groceries VALUES (1, "Peanut Butter", 1, 4);
 INSERT INTO groceries VALUES (2, "Apples", 5, 1);
+INSERT INTO groceries VALUES (3, "Tea", 2, 3);
+
 
 SELECT * FROM groceries WHERE quantity > 1 ORDER BY aisle;
 
