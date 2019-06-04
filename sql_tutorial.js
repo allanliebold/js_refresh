749d9324a64cb8d487bf157cc05b8ccdedb53416
Allan Liebold <allan.liebold@gmail.com>
2019-06-04 06:20:26 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -188,6 +188,7 @@ CREATE TABLE student_projects (id INTEGER PRIMARY KEY,
     
 INSERT INTO student_projects (student_id, title)
     VALUES (1, "");
+
 SELECT students.first_name, students.last_name, student_projects.title
     FROM students
     JOIN student_projects
