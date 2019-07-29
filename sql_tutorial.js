dc9f54b139777a02b52c8d8ff891f7a27f314a19
Allan Liebold <allan.liebold@gmail.com>
2019-07-29 05:22:53 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -8,7 +8,7 @@ INSERT INTO groceries VALUES (4, "Bread", 1, 4);
 INSERT INTO groceries VALUES (5, "Bananas", 4, 1);
 INSERT INTO groceries VALUES (6, "Cereal", 2, 3);
 INSERT INTO groceries VALUES (7, "Eggs", 6, 6);
-INSERT INTO groceries VALUES (9, "Tuna", 5, 5);
+INSERT INTO groceries VALUES (10, "Tuna", 5, 5);
 INSERT INTO groceries VALUES (10, "Chili", 2, 5);
 INSERT INTO groceries VALUES (11, "Cheese", 1, 6);
 
