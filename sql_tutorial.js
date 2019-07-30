3c1b0ebae31e84dd8309731cf5556acaa53b29eb
Allan Liebold <allan.liebold@gmail.com>
2019-07-30 12:22:12 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -209,6 +209,5 @@ SELECT DISTINCT job_title
   FROM staff;
 // Display each unique job title contained in the staff database, disregarding repeated instances of the same title. 
 
-SELECT ALL customerid, item, price
-  FROM items_ordered
+SELECT * FROM items_ordered
   WHERE customerid = 10449;
