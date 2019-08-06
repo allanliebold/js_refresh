39dad6ba39fffdf39acf9f993c7f10c926b476ae
Allan Liebold <allan.liebold@gmail.com>
2019-08-06 06:02:54 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -253,3 +253,9 @@ SELECT employeeid, age, lastname, salary
 SELECT customer_info.firstname, customer_info.lastname, purchases.item
   FROM customer_info, purchases
   WHERE customer_info.customer_number = purchases.customer_number;
+
+SELECT customer_info.firstname, customer_info.lastname, purchases.item
+
+FROM customer_info INNER JOIN purchases
+
+ON customer_info.customer_number = purchases.customer_number;
