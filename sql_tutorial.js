6e5a2fd407f98c1438a7ae9ae1121210b90fb0bd
Allan Liebold <allan.liebold@gmail.com>
2019-08-05 22:05:10 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -21,7 +21,7 @@ SELECT MAX(quantity) FROM groceries;
 // Returns 6, the highest number from quantity
 
 INSERT INTO groceries VALUES (4, "Bananas", 5, 1);
-SELECT aisle, SUM(quantity) FROM groceries; 
+  SELECT aisle, SUM(quantity) FROM groceries; 
 
 /* This will list out the aisle numbers in one column and the sum of each aisle in the second
 
