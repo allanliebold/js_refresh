ab67864dc09ba93a70efb32360995932c22cde60
Allan Liebold <allan.liebold@gmail.com>
2019-08-06 06:03:18 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -256,5 +256,4 @@ SELECT customer_info.firstname, customer_info.lastname, purchases.item
 
 SELECT customer_info.firstname, customer_info.lastname, purchases.item
 FROM customer_info INNER JOIN purchases
-
 ON customer_info.customer_number = purchases.customer_number;
