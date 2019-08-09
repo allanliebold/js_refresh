1ab11c024b47d72e541786485c8238d8fd43f927
Allan Liebold <allan.liebold@gmail.com>
2019-08-08 21:29:50 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -35,6 +35,7 @@ CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER);
 
 INSERT INTO books VALUES (1, "A Knight of the Seven Kingdoms", 4);
 INSERT INTO books VALUES (2, "Dune", 4);
+INSERT INTO books VALUES ();
 
 CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
 INSERT INTO todo_list VALUES (1, "Laundry", 90);
