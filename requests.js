8e0160c63a948b0ee5a34e00a9b260bc52981988
Allan Liebold <allan.liebold@gmail.com>
2019-08-08 21:45:40 -0700

    Update requests.js

--- requests.js
+++ requests.js
@@ -9,6 +9,7 @@ const xhr = new XMLHttpRequest;
 const url = 'https://api-to-call.com/endpoint';
 
 xhr.responseType = 'json';
+
 xhr.onreadystatechange = () => {
   if(xhr.readyState === XMLHttpRequest.DONE) {
     return xhr.response;
