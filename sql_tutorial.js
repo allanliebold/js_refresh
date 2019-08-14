82fcccc34454fc423d10127f21477b029fd35d11
Allan Liebold <allan.liebold@gmail.com>
2019-08-14 06:19:21 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -295,4 +295,4 @@ SELECT sum(theaters.screens), movies.title
 
 SELECT employees.id, employees.firstname, employees.lastname
   FROM employee_info
-  WHERE employees.firstname LIKE '';
+  WHERE employees.firstname LIKE 'A%';
