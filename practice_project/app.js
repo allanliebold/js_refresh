8fe5455da97a903766c6638e349d728de3180aca
Allan Liebold <allan.liebold@gmail.com>
2019-08-15 23:38:53 -0700

    Update app.js

--- practice_project/app.js
+++ practice_project/app.js
@@ -49,6 +49,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
     document.querySelector('.dice-1').style.display = 'none';
     document.querySelector('.dice-2').style.display = 'none';
+    
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
     gamePlaying = false; // end the game
