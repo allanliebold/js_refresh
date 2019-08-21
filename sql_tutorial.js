ed4ad8402308cc81ba78f7d2f025f5e1b6835d4b
Allan Liebold <allan.liebold@gmail.com>
2019-08-20 22:44:20 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -178,7 +178,6 @@ INSERT INTO hobbies (person_id, name) VALUES (3, "skating");
 INSERT INTO hobbies (person_id, name) VALUES (3, "rowing");
 INSERT INTO hobbies (person_id, name) VALUES (3, "drawing");
 INSERT INTO hobbies (person_id, name) VALUES (4, "meowing");
-INSERT INTO hobbies (person_id, name) VALUES (4, "napping");
 
 CREATE TABLE student_projects (id INTEGER PRIMARY KEY,
     student_id INTEGER,
