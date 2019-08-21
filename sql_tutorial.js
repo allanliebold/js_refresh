13e9ae11db3e616f64e444a34cbd460205e838d4
Allan Liebold <allan.liebold@gmail.com>
2019-08-20 21:59:05 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -39,7 +39,6 @@ INSERT INTO books VALUES (4, "The Divine Comedy", 2);
 INSERT INTO books VALUES (5, "Dune Messiah", 3);
 INSERT INTO books VALUES (6, "The Hobbit", 7);
 INSERT INTO books VALUES (7, "Mary Poppins", 1);
-INSERT INTO books VALUES (8, "L.A. Confidential", 3);
 
 CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
 INSERT INTO todo_list VALUES (1, "Laundry", 90);
