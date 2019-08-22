c8d2c93e481a69acaf89e70eb042005bfb51e3d6
Allan Liebold <allan.liebold@gmail.com>
2019-08-22 15:06:53 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -320,3 +320,7 @@ SELECT name, population, area
 SELECT name, ROUND(population / 1000000, 2), ROUND(gdp / 1000000000, 2)
      FROM world
      WHERE continent = 'South America';
+
+SELECT name, ROUND(gdp / population, -3)
+     FROM world
+     WHERE gdp > 1000000000000;
