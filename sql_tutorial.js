729125999b5c12015a6ea80029b82fe19a75f703
Allan Liebold <allan.liebold@gmail.com>
2019-08-22 14:08:08 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -300,3 +300,7 @@ SELECT name, population/area
 SELECT name, gdp/population
      FROM world
      WHERE population > 200000000;
+
+SELECT name, population / 1000000
+     FROM world 
+     WHERE continent = 'South America';
