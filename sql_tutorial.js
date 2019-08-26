9264973814236051477f924428738a482b968058
Allan Liebold <allan.liebold@gmail.com>
2019-08-25 21:30:17 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -260,9 +260,6 @@ SELECT customer_info.firstname, customer_info.lastname, purchases.item
   FROM customer_info INNER JOIN purchases
   ON customer_info.customer_number = purchases.customer_number;
 
-SELECT employee_info.employeeid, employee_info.lastname, employee_sales.comission
-  FROM employee_info, employee_sales
-  WHERE employee_info.employeeid = employee_sales.employeeid;
 
 SELECT customers.customerid, customers.firstname, customers.lastname,  
   items_ordered.order_date, items_ordered.item, items_ordered.price
