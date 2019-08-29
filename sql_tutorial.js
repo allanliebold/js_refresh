09288094eeddcfc4b7b72c3f0360104a9f7ccc15
Allan Liebold <allan.liebold@gmail.com>
2019-08-29 14:14:14 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -334,3 +334,6 @@ SELECT name, continent FROM world
      WHERE continent IN (SELECT continent FROM world
                           WHERE name = 'Argentina' OR name = 'Australia')
      ORDER BY name;
+
+SELECT name, population FROM world
+     WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland')
