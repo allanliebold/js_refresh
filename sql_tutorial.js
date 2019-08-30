9057578d01e562d62118d3ddfdb28e131bc8b2a0
Allan Liebold <allan.liebold@gmail.com>
2019-08-30 06:02:18 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -336,4 +336,5 @@ SELECT name, continent FROM world
      ORDER BY name;
 
 SELECT name, population FROM world
-     WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland')
+     WHERE population > (SELECT population FROM world WHERE name = 'Canada') 
+      AND population < (SELECT population FROM world WHERE name = 'Poland')
