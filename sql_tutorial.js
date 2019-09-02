e091f2e2df8d7fca313a38cd1e12d542c7556ee9
Allan Liebold <allan.liebold@gmail.com>
2019-09-01 23:49:02 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -26,7 +26,6 @@ aisle      SUM(quantity)
 */
 
 CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER);
-
 INSERT INTO books VALUES (1, "A Knight of the Seven Kingdoms", 4);
 INSERT INTO books VALUES (2, "Dune", 4);
 INSERT INTO books VALUES (3, "Beloved", 4);
