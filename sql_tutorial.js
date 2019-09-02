e5c96410640edcc49d144b1060fc8a04a17c2973
Allan Liebold <allan.liebold@gmail.com>
2019-09-01 23:58:18 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -318,10 +318,6 @@ SELECT name FROM world
           (SELECT (gdp / population) FROM world
                 WHERE name='United Kingdom')
 
-SELECT name, continent FROM world
-     WHERE continent IN (SELECT continent FROM world WHERE name = 'Argentina' OR name = 'Australia')
-     ORDER BY name;
-
 SELECT name, population FROM world
   WHERE population > (SELECT population FROM world WHERE name = 'Canada') 
     AND population < (SELECT population FROM world WHERE name = 'Poland');
