5b42ef944ba2a1714abf91f103678cdfed9c8a71
Allan Liebold <allan.liebold@gmail.com>
2019-09-03 15:41:24 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -340,4 +340,4 @@ SELECT name, continent, population
      FROM world x
      WHERE 25000000 >= ALL(SELECT population 
                             FROM world y 
-                                                       WHERE y.continent = x.continent)
+                            WHERE y.continent = x.continent)
