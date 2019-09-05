5646106c6909656278be6869099a454132de7dd2
Allan Liebold <allan.liebold@gmail.com>
2019-09-05 16:13:47 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -382,3 +382,7 @@ WHERE coach = 'Fernando Santos';
 SELECT player 
 FROM goal JOIN game ON id = matchid
 WHERE stadium = 'National Stadium, Warsaw'
+
+SELECT teamname, COUNT(gtime)
+  FROM eteam JOIN goal ON id=teamid
+ GROUP BY teamname
