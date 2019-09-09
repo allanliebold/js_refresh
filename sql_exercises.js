b2a4f3d26393757d89055aa9d707bf8ea44aa05b
Allan Liebold <allan.liebold@gmail.com>
2019-09-09 11:42:31 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -156,8 +156,8 @@ SELECT teacher.name, dept.name
               ON (teacher.dept=dept.id);
 
 SELECT teacher.name, dept.name
-FROM teacher LEFT JOIN dept
-ON (teacher.dept=dept.id);
+  FROM teacher LEFT JOIN dept
+  ON (teacher.dept=dept.id);
 
 SELECT name
   FROM director INNER JOIN movie ON movie.id = director.id
