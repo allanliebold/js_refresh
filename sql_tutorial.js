6de0d47b3d43232b4aeef8073f03d7046d1666b6
Allan Liebold <allan.liebold@gmail.com>
2019-09-08 23:08:25 -0700

    Update sql_tutorial.js

--- sql_tutorial.js
+++ sql_tutorial.js
@@ -401,3 +401,7 @@ SELECT name FROM teacher
 SELECT teacher.name, dept.name
   FROM teacher INNER JOIN dept
               ON (teacher.dept=dept.id);
+
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+ON (teacher.dept=dept.id)
