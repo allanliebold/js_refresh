ecbb76f1b4cc8264b72b52b3c5ef99fa99ecad41
Allan Liebold <allan.liebold@gmail.com>
2019-09-12 10:23:17 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -166,6 +166,10 @@ SELECT name, COALESCE(mobile, '07986-444-2266') as mobile
 SELECT COUNT(name) AS teachers, COUNT(mobile) AS mobile
 FROM teacher
 
+SELECT dept.name, COUNT(teacher.name) 
+FROM teacher RIGHT JOIN dept ON teacher.dept = dept.id
+GROUP BY dept.name
+
 SELECT name
   FROM actor INNER JOIN movie ON actor.id = director
   WHERE gross < budget;
