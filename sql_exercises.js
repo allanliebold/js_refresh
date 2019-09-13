2e23d84d238b31add8bd63d870558030543aba27
Allan Liebold <allan.liebold@gmail.com>
2019-09-13 05:42:31 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -168,7 +168,7 @@ SELECT COUNT(name) AS teachers, COUNT(mobile) AS mobile
 
 SELECT dept.name, COUNT(teacher.name) 
 FROM teacher RIGHT JOIN dept ON teacher.dept = dept.id
-GROUP BY dept.name
+GROUP BY dept.name;
 
 SELECT name
   FROM actor INNER JOIN movie ON actor.id = director
