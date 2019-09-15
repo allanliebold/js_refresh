aead9ed6b8a538f31134ff409cd18870906fd6e4
Allan Liebold <allan.liebold@gmail.com>
2019-09-14 21:24:44 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -38,7 +38,7 @@ SELECT name, population, area
 
 SELECT name, ROUND(population / 1000000, 2), ROUND(gdp / 1000000000, 2)
   FROM world
-     WHERE continent = 'South America';
+  WHERE continent = 'South America';
 
 SELECT yr, subject, winner
 FROM nobel
