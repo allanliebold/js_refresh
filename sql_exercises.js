bee236ae0042f64a9dfbb070a5461e78a2827705
Allan Liebold <allan.liebold@gmail.com>
2019-09-16 06:14:10 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -185,7 +185,7 @@ SELECT teacher.name, dept.name
   ON (teacher.dept=dept.id);
 
 SELECT teacher.name, dept.name
-FROM teacher RIGHT JOIN dept ON teacher.dept=dept.id;
+  FROM teacher RIGHT JOIN dept ON teacher.dept=dept.id;
 
 SELECT name, COALESCE(mobile, '07986-444-2266') as mobile
   FROM teacher;
