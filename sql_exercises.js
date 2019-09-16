a332570a3986ef99d11b4cdfa35aef398e9096d0
Allan Liebold <allan.liebold@gmail.com>
2019-09-16 06:02:04 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -145,6 +145,34 @@ SELECT title
   FROM movie JOIN casting ON id = movieid
   WHERE actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford');
 
+SELECT name
+  FROM actor INNER JOIN movie ON actor.id = director
+  WHERE gross < budget;
+
+SELECT *
+  FROM actor JOIN casting ON actor.id = actorid
+  JOIN movie ON movie.id = movieid;
+
+SELECT name, COUNT(movieid)
+  FROM casting JOIN actor ON actorid=actor.id
+  WHERE name LIKE 'John %'
+  GROUP BY name ORDER BY 2 DESC;
+
+SELECT title 
+   FROM movie JOIN casting ON (movieid=movie.id)
+              JOIN actor   ON (actorid=actor.id)
+   WHERE name='Paul Hogan' AND ord = 1;
+
+SELECT name
+  FROM movie JOIN casting ON movie.id = movieid
+  JOIN actor ON actor.id = actorid
+  WHERE ord = 1 AND director = 351;
+
+ SELECT title, yr 
+   FROM movie, casting, actor 
+  WHERE name='Robert De Niro' AND movieid=movie.id AND actorid=actor.id AND ord = 3;
+
+
 SELECT name FROM teacher
   WHERE dept IS NULL; // = NULL doesn't work. 
 
