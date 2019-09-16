5cd6f011e3c49716accfeb8b9a1f42845d82fac0
Allan Liebold <allan.liebold@gmail.com>
2019-09-16 06:01:38 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -179,29 +179,3 @@ SELECT name,
   AS dept
   FROM teacher;
 
-SELECT name
-  FROM actor INNER JOIN movie ON actor.id = director
-  WHERE gross < budget;
-
-SELECT *
-  FROM actor JOIN casting ON actor.id = actorid
-  JOIN movie ON movie.id = movieid;
-
-SELECT name, COUNT(movieid)
-  FROM casting JOIN actor ON actorid=actor.id
-  WHERE name LIKE 'John %'
-  GROUP BY name ORDER BY 2 DESC;
-
-SELECT title 
-   FROM movie JOIN casting ON (movieid=movie.id)
-              JOIN actor   ON (actorid=actor.id)
-   WHERE name='Paul Hogan' AND ord = 1;
-
-SELECT name
-  FROM movie JOIN casting ON movie.id = movieid
-  JOIN actor ON actor.id = actorid
-  WHERE ord = 1 AND director = 351;
-
- SELECT title, yr 
-   FROM movie, casting, actor 
-  WHERE name='Robert De Niro' AND movieid=movie.id AND actorid=actor.id AND ord = 3;
