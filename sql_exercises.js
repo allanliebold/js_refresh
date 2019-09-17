a9dd785397086530539e6f79317d8155e5f10032
Allan Liebold <allan.liebold@gmail.com>
2019-09-17 12:52:17 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -207,6 +207,7 @@ SELECT name,
   AS dept
   FROM teacher;
 
-SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
+SELECT dept.name 
+  FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
 
 SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
