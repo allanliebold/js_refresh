6cc2ea48b2a24201db0fcfb28beca1891a75576f
Allan Liebold <allan.liebold@gmail.com>
2019-09-18 00:14:53 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -212,4 +212,5 @@ SELECT dept.name
   WHERE teacher.name = 'Cutflower';
 
 SELECT dept.name, COUNT(teacher.name) 
-  FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
+  FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept 
+GROUP BY dept.name
