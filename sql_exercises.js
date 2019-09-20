e9562fd36eeb331359de07284546cb3f1d1439f5
Allan Liebold <allan.liebold@gmail.com>
2019-09-20 12:56:27 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -235,3 +235,7 @@ SELECT COUNT(*)
 
              SELECT id FROM stops 
 WHERE name = 'Craiglockhart'
+
+             SELECT stops.id, stops.name 
+FROM stops JOIN route ON stops.id = route.stop
+WHERE route.company = 'LRT' AND route.num = 4
