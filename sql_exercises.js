228f84679bbaa45480d4a8ee12054cbd6f67e755
Allan Liebold <allan.liebold@gmail.com>
2019-09-24 12:49:12 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -239,4 +239,8 @@ FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopa.name='Craiglockhart';             
+WHERE stopa.name='Craiglockhart';     
+
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
