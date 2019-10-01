66383fadcd5ebe65fb4acc41c9ad37647402b24a
Allan Liebold <allan.liebold@gmail.com>
2019-10-01 14:19:16 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -246,4 +246,4 @@ SELECT company, num, COUNT(*)
 SELECT a.company, a.num, a.stop, b.stop
 FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
-WHERE a.stop=53                           
+WHERE a.stop=53 AND b.stop=149;                          
