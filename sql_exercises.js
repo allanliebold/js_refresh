e60a46ddfb9d014fbc7e6532f2f24cbdbd119a1d
Allan Liebold <allan.liebold@gmail.com>
2019-10-01 11:36:55 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -245,5 +245,6 @@ SELECT company, num, COUNT(*)
 SELECT company, num, COUNT(*)
 FROM route WHERE stop=149 OR stop=53
 GROUP BY company, num
+HAVING COUNT(*) = 2;
                            
                            
