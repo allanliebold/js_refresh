ee2567c6f13629666f81a4226154f91e366d5bb1
Allan Liebold <allan.liebold@gmail.com>
2019-10-01 14:17:52 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -241,7 +241,7 @@ FROM route a JOIN route b ON
 SELECT company, num, COUNT(*)
   FROM route WHERE stop=149 OR stop=53
   GROUP BY company, num
-HAVING COUNT(*) = 2;
+  HAVING COUNT(*) = 2;
                            
 SELECT a.company, a.num, a.stop, b.stop
 FROM route a JOIN route b ON
