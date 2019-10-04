16e79d25147eae38b4b1ea6d9ceaca3deb112fc1
Allan Liebold <allan.liebold@gmail.com>
2019-10-03 19:04:52 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -249,7 +249,7 @@ SELECT a.company, a.num, a.stop, b.stop
   WHERE a.stop=53 AND b.stop=149;
 
 SELECT a.company, a.num, stopa.name, stopb.name
-FROM route a JOIN route b ON
+  FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
