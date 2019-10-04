8c0f2f960f46a58575bd3be043b71410cc5ede54
Allan Liebold <allan.liebold@gmail.com>
2019-10-03 18:15:58 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -253,7 +253,7 @@ FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopa.name='Craiglockhart' AND stopb.name='London Road';
+  WHERE stopa.name='Craiglockhart' AND stopb.name='London Road';
 
 SELECT DISTINCT a.company, a.num
   FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
