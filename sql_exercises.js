32c7609fcae5a04c5d47e4792a33893adf9ccba8
Allan Liebold <allan.liebold@gmail.com>
2019-10-07 06:05:11 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -5,7 +5,7 @@ SELECT customer_info.firstname, customer_info.lastname, purchases.item
 
 SELECT customer_info.firstname, customer_info.lastname, purchases.item
   FROM customer_info INNER JOIN purchases
-  ON customer_info.customer_number = purchases.customer_number;
+
 
 SELECT customers.customerid, customers.firstname, customers.lastname,  
   items_ordered.order_date, items_ordered.item, items_ordered.price
