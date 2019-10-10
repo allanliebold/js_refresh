db33e4efcb87fa6b65262ff311b0661a6ab18655
Allan Liebold <allan.liebold@gmail.com>
2019-10-09 23:36:25 -0700

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -6,7 +6,9 @@ console.log(name[0] + ' was born in ' + years[0]);
 
 var types = [true, 10, 'string'];
 types.push('blue'); // adds element at end of array
+
 console.log(types);
+
 types.unshift(5); // adds element at beginning of array
 types.pop(); // removes element at end of array
 types.shift(); // removes element at beginning of array
