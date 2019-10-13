43d107a0ff47c0e50bbb1807626f84b09e6fbdc4
Allan Liebold <allan.liebold@gmail.com>
2019-10-12 23:59:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,8 @@ document.querySelector('.btn-roll').addEvenListener('click', function() {
  if(gamePlaying) {
     var dice1 = Math.floor(Math.random() * 6) + 1;
     var dice2 = Math.floor(Math.random() * 6) + 1;
-
+    
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    
 });
