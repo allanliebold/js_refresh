e617276f815a3e3e8d67bb2bd2d64e96d8b74529
Allan Liebold <allan.liebold@gmail.com>
2019-10-26 23:59:06 -0700

    Update sql_exercises.js

--- sql_exercises.js
+++ sql_exercises.js
@@ -4,6 +4,9 @@ SELECT customer_info.firstname, customer_info.lastname, purchases.item
   FROM customer_info, purchases
   WHERE customer_info.customer_number = purchases.customer_number;
 
+SELECT customer_info
+
+
 SELECT customers.customerid, customers.firstname, customers.lastname,  
   items_ordered.order_date, items_ordered.item, items_ordered.price
   FROM customers, items_ordered
