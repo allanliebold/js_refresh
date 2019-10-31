dd9233bd36e74a08d1d871030b5e12bd2cbee803
Allan Liebold <allan.liebold@gmail.com>
2019-10-30 22:20:30 -0700

    Update app2.js

--- sqlite/app2.js
+++ sqlite/app2.js
@@ -1,7 +1,6 @@
 // SQLite tutorial 2
 const { calculateAverages, addClimateRowToObject, logNodeError, printQueryResults } = require('./utils');
 const sqlite = require('sqlite3');
-
 const db = new sqlite.Database('./db.sqlite');
 const temperaturesByYear = {};
 
